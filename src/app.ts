5d9442c642db697ce5baa1e40153aeaa2b4d14af
BreMea <63671187+BreMea@users.noreply.github.com>
2022-02-26 17:03:49 -0500

    update to only get unique connections

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import {createSession} from 'pcap';
+import {createSession, decode} from 'pcap';
 import petitio from 'petitio';
 (await import('dotenv')).config();
 
-let ppm = 0;
+let ppm: string[] = [];
 
 const pcap = createSession('en0', {});
-pcap.on('packet', () => ppm++);
+pcap.on('packet', (p) => ppm.push(decode.packet(p).payload.shost.addr.join('.')));
 
 setInterval(() => {
 	const rr = petitio(process.env.URL!);
 	rr.method('POST');
-	rr.body({}, 'json');
+	rr.body({unique: ppm.filter((v, i, a) => a.indexOf(v) === i).length}, 'json');
 	rr.header('Authorization', process.env.AUTH!);
 	rr.send();
-	ppm = 0;
+	ppm = [];
 }, 60 * 1000);
